da7c675d508fe74c7071df6a4e17291fd2cc4055
ICS-etienne <etienne.delagneau@ecole-air.fr>
2025-04-23 14:57:12 +0200

    architecture site

---
+++ src/content/config.ts
@@ -0,0 +1,68 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const actualites = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/blog-actualites" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    categories: z.array(z.string()),
+    author: z.string(),
+    summary: z.string(),
+    picture: z.string().optional(),
+  }),
+});
+
+const agenda = defineCollection({
+  loader: glob({ pattern: "**/*.json", base: "src/content/timelines-agenda" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.string().transform((val) => new Date(val)),
+    end_date: z.string().optional().transform((val) => val ? new Date(val) : undefined),
+    categories: z.array(z.string()).optional(),
+    author: z.string().optional(),
+    summary: z.string().optional(),
+    picture: z.string(),
+    city: z.string(),
+    country: z.string(),
+    link: z.string().optional(),
+  }),
+});
+
+  const ressources = defineCollection({
+    loader: glob({ pattern: "**/*.json", base: "src/content/masonry-ressources" }),
+    schema: z.array(
+      z.object({
+        title: z.string(),
+        links: z
+          .array(
+            z.object({
+              title: z.string(),
+              link: z.string(),
+              description: z.string().optional()
+            })
+          )
+          .optional(),
+        children: z
+          .array(
+            z.object({
+              title: z.string(),
+              links: z.array(
+                z.object({
+                  title: z.string(),
+                  link: z.string(),
+                  description: z.string().optional()
+                })
+              )
+            })
+          )
+          .optional(),
+      })
+    ),
+  });
+
+export const collections = {
+  actualites,
+  agenda,
+  ressources,
+};
